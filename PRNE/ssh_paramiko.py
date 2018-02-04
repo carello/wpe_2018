c0f7f303af33c76a447095ce0d7c44667d9bb0c6
carello <cp2carello@gmail.com>
2018-02-04 12:54:24 -0600

    ssh_paramkio added comments

--- PRNE/ssh_paramiko.py
+++ PRNE/ssh_paramiko.py
@@ -12,8 +12,7 @@ print('--- Attempting connection to {}'.format(ip_address))
 
 ssh_client = paramiko.SSHClient()
 
-# Must set missing host key policy since we don't have the SSH key
-# stored in the 'known_hosts' file
+# Must set missing host key policy since we don't have the SSH key stored in the 'known_hosts' file
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # Make the connection to our host.
@@ -29,6 +28,7 @@ print('------------------------------------------------------\n')
 
 # Execute some commands
 stdin, stdout, stderr = ssh_client.exec_command('show ip int brief')
+
 output = stdout.readlines()
 data = [line.strip('\n') for line in output]
 for d in data:
