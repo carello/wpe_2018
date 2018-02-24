20b196aa91c6fe98ed250b237f37e4143042d13b
carello <cp2carello@gmail.com>
2018-02-24 16:16:58 -0600

    comments and templates to wedding planner
    	modified:   wedding_planner/solution_wedding1.py
    	new file:   wedding_planner/template.py
    	new file:   wedding_planner/template2.py

---
+++ wedding_planner/template.py
@@ -0,0 +1,155 @@
+# Overall Objective
+# Write a class to organize wedding guests.
+# The guests themselves are represented by named tuples,
+# and the tables at which they were seated are represented by integers.
+# The idea is for the class to help us to arrange the guests at the wedding by table.
+# We're going to do that with a GuestList class. We'll create an instance of this class,
+# and add our guests (as named tuples) into the guest list, along with a table number.
+# We'll then be able to ask our class for a complete guest list by table, as well as a
+# report of which tables aren't yet full.
+#
+# - Create a GuestList class
+# - Create an instance of this class, add our guests (as named tuples) to the guest list, along with a table num.
+#   Assume everyone has a unique name.
+# - Create reports of: complete guest list by table and a report of which tables aren't full.
+
+
+# ========================= INSTRUCTIONS==================================
+#
+# Example how this should work:
+# Person should be a named tuple, with "first" and "last" attributes
+#   gl = GuestList()
+#   gl.assign(Person('Waylon', 'Dalton'), 1)
+#   gl.assign(Person('Justine', 'Henderson'), 1)
+#   gl.assign(Person('Abdullah', 'Lang'), 3)
+#   gl.assign(Person('Mathias', 'Little'), 2)
+#   gl.assign(Person('Eddie', 'Randolph'), None)
+#
+# Now we want to run reports
+# 1) How many guests total? run: len(gl) to get an integer back.
+# 2) What guests are at a given table? run: gl.table(2) to get a list of of Person object back.
+# 3) What guests aren't assigned to any table? run: gl.unassigned() to get a list of Person object back
+#
+# 4) Given a Person object, we should be able to assign them to a table:
+#   p = Person('Joanna', 'Shaffer')
+#   gl.assign(p, 3)
+# If the person is already in the system, but is assigned to another table (or to no table at all),
+# then they will now be assigned to a new table.
+# If the person isn't already in the system, then they will be added and then assigned to a table.
+# If there is no room at the table (i.e., there are already 10 guests), then we should raise a TableFull exception.
+#
+# 5) We should also be able to learn how much space is available at each table: gl.free_space()
+# Returning The a dictionary of table names (keys) and remaining space (values) for each table.
+#
+# 6) Run: gl.guest() and get a list of all guests, sorted first by table number, then last name, then first name.
+#
+# 7) Finally run: print(gl) and get a nicely printed output as:
+#    2
+#        last, first
+#        last, first
+#    3
+#        last, first
+#        last, first
+#    4
+#        last, first
+#        last, first
+#        last, first
+#
+# ==============================================================================
+
+# import
+
+# Create a named tuple. Note namedtuple actually returns a new 'class'.
+# Creating a class called Person (right hand side) and assigning to Person (left side)
+
+
+
+# If there is no room at the table (i.e., there are already 10 guests), then we should raise a TableFull exception.
+
+
+# Create Guestlist
+
+
+
+    # Assign people to a table using append, also check if there's space at the table
+
+
+        # RE-ASSIGN: check tables; remove if person is there
+
+        # Assign to table
+
+
+    # Find out how many guests there are (total).
+
+
+    # Search tables to find which guests are assigned
+
+
+    # Find which people are unassigned
+
+
+    # Find free space. Ignore 'None'. Return as a dictionary
+
+
+    # Obtain guest list by: table num, last, first
+
+
+    # Output printint format
+
+
+gl = GuestList()
+gl.assign(Person('Waylon', 'Dalton'), 1)
+gl.assign(Person('Justine', 'Henderson'), 1)
+gl.assign(Person('Abdullah', 'Lang'), 3)
+gl.assign(Person('Marcus', 'Cruz'), 1)
+gl.assign(Person('Thalia', 'Cobb'), 2)
+gl.assign(Person('Mathias', 'Little'), 2)
+gl.assign(Person('Eddie', 'Randolph'), None)
+gl.assign(Person('Angela', 'Walker'), 2)
+gl.assign(Person('Lia', 'Shelton'), 3)
+gl.assign(Person('Hadassah', 'Hartman'), None)
+gl.assign(Person('Joanna', 'Shaffer'), 3)
+gl.assign(Person('Jonathon', 'Sheppard'), 2)
+
+print()
+print('-------- THE WEDDING PLANNER --------')
+print('\nTotal Guests')
+print(len(gl))
+print('-' * 80)
+
+print('\nGuests at table 2: ')
+print(gl.table(2))
+print('-' * 80)
+
+print('\nUnassigned guests:')
+print(gl.unassigned())
+print('-' * 80)
+
+print('\nTotal guests a table 2:')
+print(len(gl.table(2)))
+print('\nTotal guests a table 3:')
+print(len(gl.table(3)))
+print('-' * 80)
+
+print('\nRe-assign Joanna to table 2')
+p = Person('Joanna', 'Shaffer')
+gl.assign(p, 2)
+print('\nTotal guests a table 2:')
+print(len(gl.table(2)))
+print('\nTotal guests a table 3:')
+print(len(gl.table(3)))
+print('-' * 80)
+
+print('\nFree Space for all tables')
+print(gl.free_space())
+print('-' * 80)
+
+print('\nPrint out all guests:')
+print()
+for one_guest in gl.guests():
+    print(one_guest)
+
+print('-' * 80)
+print('\nFormat print: each table by guests\n')
+print(gl)
+print()
--- wedding_planner/template2.py
+++ wedding_planner/template2.py
@@ -1,4 +1,3 @@
-
 # Overall Objective
 # Write a class to organize wedding guests.
 # The guests themselves are represented by named tuples,
@@ -62,105 +61,67 @@
 #
 # ==============================================================================
 
-
-
-from collections import namedtuple
-from collections import defaultdict
+# import
 
 # Create a named tuple. Note namedtuple actually returns a new 'class'.
 # Creating a class called Person (right hand side) and assigning to Person (left side)
 Person = namedtuple('Person', ['first', 'last'])
 
-# Define class GuestList. This class allows us to define a guest list,
-# assigning people to tables and producing reports that describe how many
-# guests are at each table. Each instance of "GuestList" is going to need
-# to keep track of any number of guests at a table and most of our interactions
-# are going to involve the table numbers. Suggest that we store guest information
-# in a dictionary,in which the table number is the key and the people sitting at
-# that table are in a list. Each value will thus be a list of "Person" named tuples.
-# If not assigned to a table, use 'None' - note that 'None' can be a dictionary key.
-#
-# Now, while we could use a regular dictionary to store our people,
-# we'll be better off with a "defaultdict".  That's because we can initialize
-# a defaultdict with a callable which is invoked each time a key doesn't exist.
-# Given that we'll use a list as the value for each table, create a defaultdict of lists.
-# Moreover, set that to be the "tables_and_guests" attribute in __init__,
-# so that every time we create a new guest list, we'll have our defaultdict ready to go
 
-
+# If there is no room at the table (i.e., there are already 10 guests), then we should raise a TableFull exception.
 class TableFull(Exception):
     pass
 
 
+# Create Guestlist
 class GuestList(object):
-    max_at_table = 10
 
     def __init__(self):
-        self.tables_and_guests = defaultdict(list)
+        pass
 
-    # Create an assign method to assign someone to a table. thanks to defaultdict
-    # since new tables_and_guests is a list and we can append 'person'.
-    # We can reassign people, so we'll need to search through the entire guest list.
-    def assign(self, person, new_table_number):
-        # Check if there's space at table.
-        if len(self.tables_and_guests[new_table_number]) == GuestList.max_at_table:
-            raise TableFull('NO ROOM AT THAT TABLE!')
+    # Assign people to a table using append
+    # 1) Check if there's space at the table
+    def assign(self, person, new_table):
+        pass
 
-        # Look through existing tables; remove if the person is there.
-        for table_number, guests in self.tables_and_guests.items():
-            if person in guests:
-                guests.remove(person)   # TBD
-                break
-        # Add the person to the new table
-        self.tables_and_guests[new_table_number].append(person)
+        # 2) RE-ASSIGN: check tables; remove if person is there
 
-    # Find out how many guests are there in total.
+
+        # 3) Assign to table
+
+
+
+    # Find out how many guests there are (total).
     def __len__(self):
-        return sum([len(one_table)
-                    for one_table in
-                    self.tables_and_guests.values()])
+        pass
+
 
     # Search tables to find which guests are assigned
     def table(self, table_number):
-        return self.tables_and_guests[table_number]
+        pass
 
-    # Find people who are unassigned to a table
+
+    # Find which people are unassigned
     def unassigned(self):
-        return self.table(None)
+        pass
 
-    # Find free space at a table, return a dictionary using dict comprehension.
-    # We can ignore 'None'.
+
+    # Find free space. Ignore 'None'. Return as a dictionary
     def free_space(self):
-        return {table_number: GuestList.max_at_table - len(guests)
-                for table_number, guests in self.tables_and_guests.items()
-                if table_number}
+        pass
 
-    # This next one is a tough one.
-    # Obtain guest list, sorted by table, last_name, first_name. Problem is:
-    # there's no table number attribute. So we need to flip this where: each
-    # key is a Person namedtuple and value is the table number.
-    # We have to account for guest not assigned (None) also.
+
+
+    # Obtain guest list by: table num, last, first
     def guests(self):
-        guests_dict = {one_guest: table_number
-                       for table_number, guests_at_table in
-                       self.tables_and_guests.items()
-                       for one_guest in guests_at_table}
-        return sorted(guests_dict.keys(),
-                      key=lambda g: (guests_dict[g] or -1, g.last, g.first))
+        pass
 
-    # Printing format output with tables and guests per table. This is a mess
-    # and not understandable.
+    # Output printint format
     def __repr__(self):
-        output = ''
-        for table_number, guests_at_table in sorted(self.tables_and_guests.items(),
-                                                    key=lambda t: t[0] or -1):
-            output += '{0}\n'.format(table_number)
+        pass
 
-            for one__guest in sorted(guests_at_table,
-                                    key=lambda t: t[::-1]):
-                output += '\t{0}, {1}\n'.format(one__guest.last, one__guest.first)
-        return output
 
+# ==============================
 
 gl = GuestList()
 gl.assign(Person('Waylon', 'Dalton'), 1)
@@ -218,6 +179,3 @@ print('-' * 80)
 print('\nFormat print: each table by guests\n')
 print(gl)
 print()
-
-
-
