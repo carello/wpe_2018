a9a206a2194dfecb4e352fc8c228f27a1a46559d
carello <cp2carello@gmail.com>
2018-08-31 16:05:55 -0500

    addedd mypy_payment

---
+++ mypy_payment/pre35.1.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+from abc import ABCMeta, abstractmethod
+import random
+
+class AbstractPayment(metaclass=ABCMeta):
+    @abstractmethod
+    def authorize_payment(self, user_id:int,
+                          amount:int, currency:str) -> bool:
+        pass
+
+    @abstractmethod
+    def charge_payment(self, user_id:int, amount:int,
+                       currency:str, merchant_id:int) -> int:
+        pass
+
+    @abstractmethod
+    def reverse_payment(self, user_id:int, amount:int,
+                        currency:str, merchant_id:int) -> int:
+        pass
+
+class CannotChargeError(Exception):
+    pass
+
+class CannotReverseError(Exception):
+    pass
+
+class MyNewPayment(AbstractPayment):
+    def authorize_payment(self, user_id:int,
+                          amount:int, currency:str):
+        return True
+
+    def charge_payment(self, user_id:int, amount:int,
+                       currency:str, merchant_id:int):
+        if amount < 0 or amount > 1000:
+            raise CannotChargeError(f"user_id {user_id}, amount {amount}, currency {currency}, merchant_id {merchant_id}")
+        
+        return random.randint(0, 100000)
+
+    def reverse_payment(self, user_id:int, amount:int,
+                        currency:str, merchant_id:int):
+        if amount < 0 or amount > 1000:
+            raise CannotReverseError(f"user_id {user_id}, amount {amount}, currency {currency}, merchant_id {merchant_id}")
+        
+        return random.randint(0, 100000)
+
+mnp = MyNewPayment()
+authorize_result = mnp.authorize_payment(1, 100, 'usd')
+print(f"authorize_result = {authorize_result}")
+
+print("Charging")
+charge_result = mnp.charge_payment(1, 100, 'usd', 2)
+print(f"charge_result = {charge_result}")
+
+try:
+    charge_result = mnp.charge_payment(1, -5, 'usd', 2)
+    print(f"charge_result = {charge_result}")
+except CannotChargeError as e:
+    print(f"Cannot charge: {e}")
+
+try:
+    charge_result = mnp.charge_payment(1, 2000, 'usd', 2)
+    print(f"charge_result = {charge_result}")
+except CannotChargeError as e:
+    print(f"Cannot charge: {e}")
+    
+print("Reversing")
+reverse_result = mnp.reverse_payment(1, 100, 'usd', 2)
+print(f"reverse_result = {reverse_result}")
+
+try:
+    reverse_result = mnp.reverse_payment(1, -5, 'usd', 2)
+    print(f"reverse_result = {reverse_result}")
+except CannotReverseError as e:
+    print(f"Cannot reverse: {e}")
+
+try:
+    reverse_result = mnp.reverse_payment(1, 2000, 'usd', 2)
+    print(f"reverse_result = {reverse_result}")
+except CannotReverseError as e:
+    print(f"Cannot reverse: {e}")
+    
